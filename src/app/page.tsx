3aca5f65129eee1c25a00a98b664c72eac1bb7bf
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-05-04 03:31:35 -0500

    AAAAAAA
    Co-authored-by: Juan Pablo Perez Alvarez <jperezalv23@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Inventory from "./components/inventory";
 import { useRouter } from 'next/navigation'; // ✅ App Router
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
